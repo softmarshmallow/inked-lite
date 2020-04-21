6e9ddf1e6d31d0f8495b7dfe145eedf735b9fe95
woojoo <woojoo@softmarshmallow.com>
2020-04-21 23:17:07 +0900

    updated alert

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <button v-on:click="playaudio">play sound</button>
   </div>
 </template>
 
 <script>
+  import io from 'socket.io-client';
+  var played = [];
+
   export default {
     name: 'inked-lite',
-    methods: {
-      playaudio: function () {
+    mounted: function (){
+      const socket = io('http://13.209.232.176:3001/client');
+      // var socket = io.connect()
+      socket.on("alert", function (id) {
+        if (played.includes(id)){
+          return
+        }
+        played.push(id)
         var audio = new Audio('http://13.209.232.176:3000/static/sound-effects/tone-1.wav');
         audio.play();
-      }
+
+        if (played.lenght > 1000){
+          played = [];
+        }
+      })
+    },
+    methods: {
+
     }
   }
 </script>
